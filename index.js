75ad637c0dceb22fe925cbffbffb6bb7e858e77c
Michael Wong <mik9080@gmail.com>
2017-10-11 17:27:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array,fn){
+  array.forEach(fn);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element,index,array){
+  array[index] += "a"
+}
